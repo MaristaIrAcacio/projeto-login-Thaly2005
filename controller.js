6cc07d4d65cf68977550c023be74e35e24c04360
Thalyta das Neves Ferreira <thalyta.ferreira@alunoescolassociais.org.br>
2023-07-26 11:51:08 -0300

    Modifiquei, concertei e acrescentei

--- controller.js
+++ controller.js
@@ -1,10 +1,93 @@
+/* FUNÇÃO PARA VERIFICAR PREENCHIMENTO DE LOGIN*/
+
 function acessar() {
+
     let loginEmail = document.getElementById("loginEmail").value;
+
     let loginSenha = document.getElementById("loginSenha").value;
+
  
+
     if(!loginEmail || !loginSenha) {
+
         alert("favor preencha os campos para acessar o site");
+
     } else {
-       window.location.href = "aula2.html";
+
+            window.location.href = "aula2.html";
+
     }
+
+}
+
+
+
+
+// CRIA LISTA DE USUÁRIO DENTRO DE UM ARRAY
+
+
+
+
+var dadosLista = [];
+
+
+
+
+function salvarUser(){
+
+    let nomeUser = document.getElementById("nomeUser").value;
+
+
+
+
+    if(nomeUser){
+
+        dadosLista.push(nomeUser);
+
+        criaLista();
+
+        document.getElementById("nomeUser").value = '';
+
+    }else{
+
+        alert("Usuário, por favor preencher o campo nome");
+
+    }
+
+}
+
+
+
+
+// FUNÇÃO PARA CRIAÇÃO DE LISTA NA TABELA
+
+function criaLista(){
+
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+
+
+
+
+    // LAÇO DE REPETIÇÃO PARA COLOCAR NOMES NA TABELA
+
+    for(let i=0;i <= (dadosLista.length-1);i++){
+
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button></td></tr>";
+
+        document.getElementById("tabela").innerHTML = tabela;
+
+    }
+
+}
+
+// FUNÇÃO PARA EDITAR NOME
+function editar(i){
+    document.getElementById("nomeUser"). value = dadosLista [(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+}
+
+// FUNÇÃO PARA EXCLUIR NOME
+function excluir(i){
+    dadosLista.splice[(i - 1), 1];
+    document.getElementById('tabela').deleteRow(i);
 }
